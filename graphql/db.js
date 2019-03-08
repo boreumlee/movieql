b019a8d04161b31fa3e4101e63122ddb7dc5dcd5
Luna <boreum4151@gmail.com>
2019-03-08 18:38:25 +0900

    add deleteMovie func

--- graphql/db.js
+++ graphql/db.js
@@ -1,21 +1,21 @@
 export let movies = [
     {
-        id: 1,
+        id: 0,
         name:"캡틴마블",
         score:1
     },
     {
-        id:2,
+        id:1,
         name:"항거",
         score:4
     },
     {
-        id:0,
+        id:2,
         name:"증인",
         score:3
     },
     {
-        id:6,
+        id:3,
         name:"극한직업",
         score:5
     }
@@ -34,4 +34,15 @@ export const deleteMovie = id =>{
     } else {
         return false;
     }
-}
+};
+
+export const addMovie=(name, score)=>{
+    const newMovie={
+        //id값 순차적으로 적용해주기 위해
+        id:`${movies.length+1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,14 @@
-import {movies, getById} from './db.js'
+import {movies, getById, addMovie, deleteMovie} from './db.js'
  
 const resolvers = {
     Query:{
         movies: () => movies,
         movie:(_,{id})=>getById(id)
     },
-    // Mutation:{
-    //     addMovie: (_,{name, score})=>
-    // }
+    Mutation:{
+        addMovie: (_,{name, score})=> addMovie(name, score),
+        deleteMovie: (_,{id})=>deleteMovie(id)
+    }
 };
 
-export default resolvers;
+export default resolvers; 
