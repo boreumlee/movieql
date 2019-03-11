cf97e0dfcc107539273a3c05ca6b92e72ab01623
Luna <boreum4151@gmail.com>
2019-03-11 16:04:24 +0900

    wrap REST API with graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import {movies, getById, addMovie, deleteMovie} from './db.js'
+import {getMovies} from './db.js'
  
 const resolvers = {
     Query:{
-        movies: () => movies,
-        movie:(_,{id})=>getById(id)
-    },
-    Mutation:{
-        addMovie: (_,{name, score})=> addMovie(name, score),
-        deleteMovie: (_,{id})=>deleteMovie(id)
+        movies: (_,{limit,rating}) => getMovies(limit, rating)
     }
 };
 
